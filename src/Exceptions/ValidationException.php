d4e7dec8b26a2489b05e53a67c48716d7d96f23d
Travis Elkins <travis.elkins+github@gmail.com>
2019-11-25 19:08:04 +0100

    Removed extra space.

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -7,7 +7,7 @@ use Exception;
 class ValidationException extends Exception
 {
     public $errors = [];
- 
+
     public function __construct(array $errors)
     {
         parent::__construct('The given data failed to pass validation.');
