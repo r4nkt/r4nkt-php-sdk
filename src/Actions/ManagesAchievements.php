d53822c64e21036ac230289074646a2a8837697b
Travis Elkins <travis.elkins+github@gmail.com>
2019-12-31 12:45:54 +0100

    Introduced ability to attach, detach, and index an achievement's rewards.

--- src/Actions/ManagesAchievements.php
+++ src/Actions/ManagesAchievements.php
@@ -2,6 +2,7 @@
 
 namespace R4nkt\PhpSdk\Actions;
 
+use R4nkt\PhpSdk\Resources\Reward;
 use R4nkt\PhpSdk\Resources\Achievement;
 
 trait ManagesAchievements
@@ -31,5 +32,26 @@ trait ManagesAchievements
     public function deleteAchievement(string $customAchievementId)
     {
         $this->delete("achievements/{$customAchievementId}");
+    }
+
+    public function attachRewardToAchievement(string $customAchievementId, string $customRewardId)
+    {
+        $data = ['custom_reward_id' => $customRewardId];
+        $rewardAttributes = $this->post("achievements/{$customAchievementId}/rewards", $data);
+
+        return new Reward($rewardAttributes['data'], $this);
+    }
+
+    public function detachRewardFromAchievement(string $customAchievementId, string $customRewardId)
+    {
+        $this->delete("achievements/{$customAchievementId}/rewards/{$customRewardId}");
+    }
+
+    public function achievementRewards(string $customAchievementId): array
+    {
+        return $this->transformCollection(
+            $this->get("achievements/{$customAchievementId}/rewards")['data'],
+            Achievement::class
+        );
     }
 }
--- src/Resources/Achievement.php
+++ src/Resources/Achievement.php
@@ -14,13 +14,34 @@ class Achievement extends ApiResource
         $this->r4nkt->deleteAchievement($this->customId);
     }
 
-    // /**
-    //  * Get the broken links for this achievement.
-    //  *
-    //  * @return array
-    //  */
-    // public function brokenLinks()
-    // {
-    //     return $this->r4nkt->brokenLinks($this->customId);
-    // }
+    /**
+     * Get the rewards for this achievement.
+     *
+     * @return array
+     */
+    public function rewards()
+    {
+        return $this->r4nkt->achievementRewards($this->customId);
+    }
+
+    /**
+     * Attach a reward to this achievement.
+     *
+     * @param  string $customRewardId [description]
+     * @return \R4nkt\PhpSdk\Resources\Reward
+     */
+    public function attachReward(string $customRewardId)
+    {
+        return $this->r4nkt->attachRewardToAchievement($this->customId, $customRewardId);
+    }
+
+    /**
+     * Detach a reward from this achievement.
+     *
+     * @return void
+     */
+    public function detachReward(string $customRewardId)
+    {
+        $this->r4nkt->detachRewardFromAchievement($this->customId, $customRewardId);
+    }
 }
